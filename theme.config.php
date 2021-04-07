fcb54d971bfc62807bc1d37396bb9fec885b545f
mis306lu <mis306@lehigh.edu>
2021-04-07 14:02:21 -0400

    initial commit

---
+++ theme.config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'extends' => 'bootstrap3',
+    'css' => array('vendor/bootstrap-slider.css',
+    			   'vendor/font-awesome.min.css',),
+    'favicon' => 'library_favicon.ico',
+ 	'js' => array(
+        'common.js',
+        'relais.js'
+    ));
